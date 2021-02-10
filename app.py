2aa3c075f35aaf7b6d768c9115c3bf93caeca9e4
Vaibhav Shukla <49766597+lksh97@users.noreply.github.com>
2021-02-11 00:38:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__) # Create object of a Flask
+
+# @ -> Annotation. What ever method you are going to route below this. The return Port no or link will render that html page
+# / here is for start(home). for starting execute this method.
+@app.route('/', methods=['GET', 'POST']) # To render Homepage
+def home_page():
+    return render_template('index.html')
+
+@app.route('/math', methods=['POST'])  # This will be called from UI
+def math_operation():
+    if (request.method=='POST'):
+        operation=request.form['operation']
+        num1=int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return render_template('results.html',result=result)
+
+# With default url add /via_postman in postman app as default url is for rendering homepage.
+# Through above url we would reach out to the below method. Here we will give Data Input through JSON.
+@app.route('/via_postman', methods=['POST']) # for calling the API from Postman/SOAPUI.
+def math_operation_via_postman():
+    if (request.method=='POST'):
+        operation=request.json['operation']
+        num1=int(request.json['num1'])
+        num2 = int(request.json['num2'])
+        if(operation=='add'):
+            r=num1+num2
+            result= 'the sum of '+str(num1)+' and '+str(num2) +' is '+str(r)
+        if (operation == 'subtract'):
+            r = num1 - num2
+            result = 'the difference of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'multiply'):
+            r = num1 * num2
+            result = 'the product of ' + str(num1) + ' and ' + str(num2) + ' is ' + str(r)
+        if (operation == 'divide'):
+            r = num1 / num2
+            result = 'the quotient when ' + str(num1) + ' is divided by ' + str(num2) + ' is ' + str(r)
+        return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
